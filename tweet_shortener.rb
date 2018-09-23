8bd0ba844ce4085cfb198188794f401275e99918
Travis Desir <travisgdesir31@gmail.com>
2018-09-23 16:04:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,8 +40,8 @@ end
     
   def shortened_tweet_truncator(tweet)
      if word_substituter(tweet).length >= 140  
-    truncated = word_substituter(tweet)[0..136].push("...").join("")
-  else 
+    shorten = word_substituter(tweet)[0..136].push("...").join("")
+    else 
     word_substituter(tweet) 
+    end 
   end 
-end 
