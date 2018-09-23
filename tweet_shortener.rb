d218365db6b161e5e80030356386688c5de8b5cd
Travis Desir <travisgdesir31@gmail.com>
2018-09-23 14:51:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,5 +4,9 @@ def dictionary
     :to || :too || :two => "2"
     :for || :four = "4"
     :be => "b"
-    
+    :you => "u"
+    :at => "@"
+    :and => "&"
   }
+  
+  def word_substituter(string)
